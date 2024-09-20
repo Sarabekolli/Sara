52f294be78eb3943e26ae3e6df64bc38da5833cd
Sarabekolli <sarabekolli7@gmail.com>
2024-09-20 18:45:46 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $x = 120;
+    $y = 50;
+
+   //echo $x + $y ; 
+   //echo $x - $y ;  
+   //echo $x * $y ;  
+  // echo $x / $y ;  
+
+ // $a = 'Digital';
+  //$b = 'School';
+  //$c = $a.$b;
+
+  //echo "$c \n";
+
+  $the_string = 'Programming';
+  echo strrev($the_string);
+ // echo str_word_count($the_string);
+
+ //$programming = 'Programming is not cool';
+ //echo str_replace("not", "very", $programming);
+
+ 
+?>
+</body>
+</html>
